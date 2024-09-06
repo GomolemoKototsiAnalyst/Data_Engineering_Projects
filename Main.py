776026fa344f585e072e6a4122344689e6dd6994
Gomolemo Kototsi <89483670+GomolemoKototsiAnalyst@users.noreply.github.com>
2024-09-06 16:28:57 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -30,19 +30,19 @@ from urllib.parse import urlparse
 warnings.simplefilter(action='ignore', category=Warning)
 
 # getting the data into the mmain: 
-loc_path = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/incident.csv'
-InProgress = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/InProgress.csv'
-Resolved = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/Resolved.csv'
-OnHold = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/OnHold.csv'
-New = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/New.csv'
-locate = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/sys_user.csv'
+#loc_path = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/incident.csv'
+#InProgress = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/InProgress.csv'
+#Resolved = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/Resolved.csv'
+#OnHold = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/OnHold.csv'
+#New = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/New.csv'
+#locate = 'https://raw.githubusercontent.com/GomolemoKototsiAnalyst/DataHub-App/main/Raw%20data/sys_user.csv'
 
-data = pd.read_csv(loc_path, encoding='ISO-8859-1')
-InProgress = pd.read_csv(InProgress, encoding='ISO-8859-1')
-Resolved = pd.read_csv(Resolved, encoding='ISO-8859-1')
-OnHold = pd.read_csv(OnHold, encoding='ISO-8859-1')
-New = pd.read_csv(New, encoding='ISO-8859-1')
-endusers_list = pd.read_csv(locate, encoding='ISO-8859-1')
+#data = pd.read_csv(loc_path, encoding='ISO-8859-1')
+#InProgress = pd.read_csv(InProgress, encoding='ISO-8859-1')
+#Resolved = pd.read_csv(Resolved, encoding='ISO-8859-1')
+#OnHold = pd.read_csv(OnHold, encoding='ISO-8859-1')
+#New = pd.read_csv(New, encoding='ISO-8859-1')
+#endusers_list = pd.read_csv(locate, encoding='ISO-8859-1')
 #loc_path = os.path.join('Raw data/incident.csv')
 #Loading the data into Python - Data Source Service Now SQL DataBase Sample size to Excel:
 #@st.cache_data

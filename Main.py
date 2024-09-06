c5887877db93d1535035d840d546c8e1547a27df
Gomolemo Kototsi <89483670+GomolemoKototsiAnalyst@users.noreply.github.com>
2024-09-06 18:42:45 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -311,21 +311,21 @@ def ITSM_Incident_Portal():
     
     #Loading the data into Python - Data Source Service Now SQL DataBase Sample size to Excel:
     #@st.cache_data
-    loc= "C:/Users/Gomolemo.Kototsi/Downloads/incident.csv"
+    loc= "Raw data/incident.csv"
     data = pd.read_csv(loc, encoding='ISO-8859-1')
     
     # Additional Files: 
-    InProgress= "C:/Users/Gomolemo.Kototsi/Downloads/InProgress.csv"
+    InProgress= "Raw data/InProgress.csv"
     InProgress = pd.read_csv(InProgress, encoding='ISO-8859-1')
 
-    Resolved= "C:/Users/Gomolemo.Kototsi/Downloads/Resolved.csv"
+    Resolved= "Raw data/Resolved.csv"
     Resolved = pd.read_csv(Resolved, encoding='ISO-8859-1')
 
-    OnHold= "C:/Users/Gomolemo.Kototsi/Downloads/OnHold.csv"
+    OnHold= "Raw data/OnHold.csv"
     OnHold = pd.read_csv(OnHold, encoding='ISO-8859-1')
 
-    New= "C:/Users/Gomolemo.Kototsi/Downloads/New.csv"
-    New = pd.read_csv(New, encoding='ISO-8859-1')
+    #New= "C:/Users/Gomolemo.Kototsi/Downloads/New.csv"
+    New = pd.read_csv('Raw data/New.csv', encoding='ISO-8859-1')
     
     #Renaming the columns for a unilateral intake:
     InProgress.rename(columns={'number': 'Number', 'due_date': 'Due date', 'short_description':'Short description','caller_id':'Caller', 'priority':'Priority',

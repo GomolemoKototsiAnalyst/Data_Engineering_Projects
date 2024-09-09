db1866b833d759a8d78d327b25352d4f7793f461
Gomolemo Kototsi <89483670+GomolemoKototsiAnalyst@users.noreply.github.com>
2024-09-09 12:08:36 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -105,6 +105,7 @@ def intro():
     with col2:
         import streamlit as st
         import streamlit.components.v1 as components
+        import os
 
         # Define the HTML and JavaScript for the 3D interactive sphere chart with an additional cylinder
         html_code = """
@@ -206,6 +207,7 @@ def intro():
     col1, col2 = st.columns([1,2])
     with col1:
         #st.markdown("Gomolemo Kototsi",unsafe_allow_html=True)
+        import os
         Gomolemo_path = os.path.join('Images', 'IMG_20211108_144305_755.webp')
         st.image(Gomolemo_path, width=600)
         #st.image('C:/Users/Gomolemo.Kototsi/Downloads/IMG_20211108_144305_755.webp', width=600)
@@ -344,7 +346,7 @@ def ITSM_Incident_Portal():
         return f"data:image/svg+xml;base64,{encoded}"
 
     #Importing data into my system: Avoiding instances where my excel file is has str headers: 
-    def read_csv_from_url(url: str, sep=',') -> pd.DataFrame:
+    def read_csv_from_url(url: str, encode='') -> pd.DataFrame:
         try:
             response = requests.get(url)
             response.raise_for_status() 
@@ -424,9 +426,9 @@ def ITSM_Incident_Portal():
     df = df_merged[df_merged['Assignment group'].str.contains("ZA - Bridge Connect|ZA - SAP Support|ZA - Cargo Wise Support|ZA - BOS Support|ZA - Carlo Support|ZA - OVB Techs|ZA - Service Desk|ZA - Infrastructure Support",case=False , na=False)]
     
     #url6=  'https://github.com/GomolemoKototsiAnalyst/DataHub-App/blob/main/Raw%20data/sys_user.csv'
-    endusers_list.rename(columns={'name': 'name'}, inplace=True)
-    endusers_list.columns = endusers_list.columns.str.strip()
-    endusers_list = endusers_list.drop_duplicates(subset='name', keep='first')
+    #endusers_list.rename(columns={'name': 'name'}, inplace=True)
+    #endusers_list.columns = endusers_list.columns.str.strip()
+    #endusers_list = endusers_list.drop_duplicates(subset='name', keep='first')
     
     # Update DataFrame Location' column based on matching 'Caller' with 'Name'
     df['country'] = df['Caller'].map(endusers_list.set_index('name')['location'])
